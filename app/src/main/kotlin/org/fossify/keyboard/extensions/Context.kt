533ec44c77066c3f37b1cd86050a7b3ff72f8da9
Kamil Vala <kamil.vala@protonmail.com>
2025-08-15 10:37:49 +0200

    feat: added czech QWERTZ layout

--- app/src/main/kotlin/org/fossify/keyboard/extensions/Context.kt
+++ app/src/main/kotlin/org/fossify/keyboard/extensions/Context.kt
@@ -46,6 +46,7 @@ import org.fossify.keyboard.helpers.LANGUAGE_BULGARIAN
 import org.fossify.keyboard.helpers.LANGUAGE_CENTRAL_KURDISH
 import org.fossify.keyboard.helpers.LANGUAGE_CHUVASH
 import org.fossify.keyboard.helpers.LANGUAGE_CZECH_QWERTY
+import org.fossify.keyboard.helpers.LANGUAGE_CZECH_QWERTZ
 import org.fossify.keyboard.helpers.LANGUAGE_DANISH
 import org.fossify.keyboard.helpers.LANGUAGE_ENGLISH_DVORAK
 import org.fossify.keyboard.helpers.LANGUAGE_ENGLISH_QWERTZ
@@ -280,6 +281,7 @@ fun Context.getKeyboardLanguageText(language: Int): String {
         LANGUAGE_CENTRAL_KURDISH -> getString(R.string.translation_central_kurdish)
         LANGUAGE_CHUVASH -> getString(R.string.translation_chuvash)
         LANGUAGE_CZECH_QWERTY -> "${getString(R.string.translation_czech)} (QWERTY)"
+        LANGUAGE_CZECH_QWERTZ -> "${getString(R.string.translation_czech)} (QWERTZ)"
         LANGUAGE_DANISH -> getString(R.string.translation_danish)
         LANGUAGE_ENGLISH_DVORAK -> "${getString(R.string.translation_english)} (DVORAK)"
         LANGUAGE_ENGLISH_QWERTZ -> "${getString(R.string.translation_english)} (QWERTZ)"
